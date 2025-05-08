586ebb0d536599c9647098fd3821999cf47d8e45
Keith Fiske <keith@keithf4.com>
2025-05-08 10:50:49 -0400

    fix: change drop order avoid dependency issue

--- updates/pgmonitor--2.1.0--2.2.0.sql
+++ updates/pgmonitor--2.1.0--2.2.0.sql
@@ -1,8 +1,8 @@
 
+DROP MATERIALIZED VIEW @extschema@.ccp_pg_settings_checksum;
 DROP VIEW @extschema@.pg_settings_hash;
 DROP FUNCTION @extschema@.pg_settings_checksum(text);
 DROP FUNCTION @extschema@.pg_settings_checksum_set_valid();
-DROP MATERIALIZED VIEW @extschema@.ccp_pg_settings_checksum;
 DELETE FROM @extschema@.metric_matviews WHERE view_name = 'ccp_pg_settings_checksum';
 DROP TABLE @extschema@.pg_settings_checksum;
 
